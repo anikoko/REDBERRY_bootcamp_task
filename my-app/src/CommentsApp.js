102f25ea35e8f6c952528376d1b8a7315de8f4b5
anikoko <anikokolishvili@gmail.com>
2025-03-21 21:30:59 +0400

    comments visual added

--- my-app/src/CommentsApp.js
+++ my-app/src/CommentsApp.js
@@ -1,5 +1,6 @@
 import './CommentsApp.css';
 import React, { useState, useEffect } from "react";
+import left from './assets/imgs/Left_2.png'
 
 function CommentsApp(props) {
   const [comments, setComments] = useState([])
@@ -36,9 +37,9 @@ function CommentsApp(props) {
   return (
     <div className='comments-box'>
         <div className='input-container'>
-            <div className='comment-input' placeholder='დაწერე კომენტარი'>
+            <textarea id="w3review" name="w3review" rows="100" cols="100" className='comment-input' placeholder='დაწერე კომენტარი'></textarea>
             <button className='write-comment-button'>დააკომენტარე</button>
-            </div>
+            
             
         </div>
         <div className='comments-text-container'>
@@ -48,12 +49,35 @@ function CommentsApp(props) {
         <div className='comments-container'>
           {comments && comments.map((comment)=>{
             const hasChild = comment.sub_comments? true : false
-            return <div>
-              <div>
-                <div className='text'>{comment.text}</div>
+            return <div className='comment-container'>
+              <div className='parent-comment-container'>
+                <div className='employee-img-container'>
+                  <img src={comment.author_avatar}></img>
+                </div>
+
+                <div className='comment-text-container'>
+                  <div className='comment-employee-name'>{comment.author_nickname}</div>
+                  <div className='the-comment-text'>{comment.text}</div>
+                  <button className='answer-button'>
+                    <img src={left} style={{width:'10px', height:'10px'}}></img>
+                    <div className='answer-button-text'>
+                      უპასუხე
+                    </div>
+                  </button>
+                </div>
+
               </div>
-              {hasChild && <div>
-                <div>{comment.sub_comments[0].text}</div>
+              
+              {hasChild && <div className='subcomment-container'>
+                <div className='employee-img-container'>
+                  <img src={comment.sub_comments[0].author_avatar}></img>
+                </div>
+
+                <div>
+                  <div className='comment-employee-name'>{comment.sub_comments[0].author_nickname}</div>
+                  <div className='subcomment'>{comment.sub_comments[0].text}</div>
+                </div>
+                
                 </div>}
               
               
